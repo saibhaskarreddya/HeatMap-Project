762039665efd62ab90a6e4197d4da4e46fa95d9d
saibhaskarreddya <163816689+saibhaskarreddya@users.noreply.github.com>
2024-03-22 15:19:13 +0530

    Git: Commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,36 +1,12 @@
 import React from 'react';
-import TwoDTable from './Screens/table';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './HomeScreen';
-import Input from './Screens/input';
-import Login from './Screens/Login';
-import Api from './Screens/Api';
-import Char from './Screens/Char';
-import HeatmapChart from './Screens/HeatMap';
+
 import MyComponent from './Screens/HeattMapp';
 
 const App = () => {
 
   return (
     <div>
-      <center>
-        <BrowserRouter>
-          <Routes>
-
-            <Route path='/' element={<Home />} />
-            <Route path='/input' element={<Input />} />
-            <Route path='/table' element={<TwoDTable />} />
-            <Route path='/Login' element={<Login />} />
-            <Route path='/Api' element={<Api />} />
-            <Route path='/Char' element={<Char/>} />
-            <Route path='/HeatmapChart' element={<HeatmapChart/>} />
-            <Route path='/HeatmapChartt' element={<MyComponent/>} />
-
-
-
-          </Routes>
-        </BrowserRouter>
-      </center>
+     <MyComponent/>
     </div>
 
   )
